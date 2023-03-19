d5935c17926dd02d534e68fa4b0ca1f64151c6ee
san <sansamaa0@gmail.com>
2023-03-20 00:07:06 +0300

    h.w

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Add your code here
+const inputForm = document.querySelector("form");
+inputForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const userName = document.querySelector("#userName").value;
+  const userEmail = document.querySelector("#userMail").value;
+  submitData(userName, userEmail);
+});
+
+async function submitData(userName, userEmail) {
+  const formData = {
+    name: userName,
+    email: userEmail,
+  };
+
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
+  const destinationURL = "http://localhost:3000/users";
+
+  try {
+    const response = await fetch(destinationURL, configurationObject);
+    const data = await response.json();
+    let p = document.createElement("p");
+    p.textContent = data.id; // use the correct property name
+    inputForm.appendChild(p);
+  } catch (error) {
+    let message = document.createElement("p");
+    message.textContent = error.message;
+    inputForm.appendChild(message);
+    return error.message;
+  }
+}
